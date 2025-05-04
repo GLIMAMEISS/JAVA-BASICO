deccf021aadffdfe3f0d64037f32cac9132011aa
GABRIELA MEISS <140433981+GLIMAMEISS@users.noreply.github.com>
2025-05-04 03:00:40 -0300

    primeiro commit

---
+++ Quiz/src/App.java
@@ -0,0 +1,9 @@
+//*  *Crie um programa com uma questão de múltipla escola */
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
