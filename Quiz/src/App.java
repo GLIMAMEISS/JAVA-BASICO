0daa4695197d18a2f3fdcca41bf475411889d3f0
GABRIELA MEISS <140433981+GLIMAMEISS@users.noreply.github.com>
2025-05-04 03:08:22 -0300

    Delete Quiz directory

--- Quiz/src/App.java
+++
@@ -1,9 +0,0 @@
-//*  *Crie um programa com uma questão de múltipla escola */
-
-
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
